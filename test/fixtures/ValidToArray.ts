12acb9d033217f74a6007b145f94b3c300de68a2
Roman Weich <roman.weich@gmail.com>
2023-08-27 16:03:03 +0200

    feat!: update major dependencies
    - adjust tests for the changes
    
    BREAKING CHANGE: will probably break as long prettier is not updated as
    well

--- test/fixtures/ValidToArray.ts
+++ test/fixtures/ValidToArray.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 const foo = Array.from([1, 2, 3]);
 const bar = [1, 2, 3];
 const baz = [...bar];
+
+/* eslint-enable @typescript-eslint/no-unused-vars */
